ccb536fb92486abb315e523dab7e74a18fb80f23
Valerie Dryden <outragedpinkracoon@gmail.com>
2018-03-19 13:59:11 +0000

    adds new views

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vuex from 'vuex'
+import EncounterBuilder from '@/components/EncounterBuilder'
 
 Vue.use(Router)
+Vue.use(Vuex)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'EncounterBuilder',
+      component: EncounterBuilder
     }
   ]
 })
