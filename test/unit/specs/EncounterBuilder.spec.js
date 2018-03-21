3fe1a9812e8bd868d51557d1d815b70c13ed2842
Valerie Dryden <outragedpinkracoon@gmail.com>
2018-03-21 09:31:00 +0000

    holy shit

---
+++ test/unit/specs/EncounterBuilder.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import EncounterBuilder from '@/components/EncounterBuilder'
+
+describe('EncounterBuilder.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(EncounterBuilder)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('h1').textContent)
+      .to.equal('Encounter Builder')
+  })
+})
