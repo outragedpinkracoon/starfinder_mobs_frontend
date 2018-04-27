9e4dc3a4ea6ab80fe3ad426777cdce3c888cf342
Valerie Dryden <outragedpinkracoon@gmail.com>
2018-04-27 14:07:02 +0100

    adds copyright

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view/>
+    <p id='footer'>This site contains Open Game Content from <a href='http://glasstopgames.com'>glasstopgames.com</a> used under the <a href='http://glasstopgames.com/open-game-license.html'>Open Game License (OGL)</a></p>
   </div>
 </template>
 
@@ -9,3 +10,9 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+#footer {
+  text-align: center;
+}
+</style>
