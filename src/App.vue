f7da4e1b5c7a34f65697e74d429387ea301fdb02
Valerie Dryden <outragedpinkracoon@gmail.com>
2018-06-21 09:26:54 +0100

    updates footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <router-view/>
-    <p id='footer'>This site contains Open Game Content from <a href='http://glasstopgames.com'>glasstopgames.com</a> used under the <a href='http://glasstopgames.com/open-game-license.html'>Open Game License (OGL)</a></p>
+    <div id='footer'>
+      <p>This site contains Open Game Content from <a href='http://glasstopgames.com'>glasstopgames.com</a> used under the <a href='http://glasstopgames.com/open-game-license.html'>Open Game License (OGL)</a>
+      </p>
+      <p>Contribute to this on <a href='https://github.com/outragedpinkracoon/starfinder_mobs_frontend'> Github! </a></p>
+      <p>API is also available for <a href='https://github.com/outragedpinkracoon/StarfinderMobsApi'>contributions</a>.</p>
+    </div>
   </div>
 </template>
 
